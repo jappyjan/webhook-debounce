a5a440b6e205f700bb64614b1d907a8e6f4db5e5
Jan Jaap <jan.jaap@digimondo.de>
2024-04-26 12:14:15 +0200

    feat: now taking get requests

--- server.js
+++ server.js
@@ -1,39 +1,93 @@
-import Koa from 'koa';
-import bodyParser from 'koa-bodyparser';
-import dotenv from 'dotenv';
-import axios from 'axios';
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import dotenv from "dotenv";
+import axios from "axios";
+
+dotenv.config();
+
+const DEBOUNCE_TIME_S = process.env.DEBOUNCE_TIME_S;
+const DEBOUNCE_TIME_MS = DEBOUNCE_TIME_S * 1000;
+
+if (!DEBOUNCE_TIME_S) {
+  console.error("DEBOUNCE_TIME_S is not set");
+  process.exit(1);
+}
+
+const PORT = process.env.PORT;
+if (!PORT) {
+  console.error("PORT is not set");
+  process.exit(1);
+}
+
+const log = (message) => console.log(`[${new Date().toISOString()}] ${message}`);
+const error = (message) => console.error(`[${new Date().toISOString()}] ${message}`);
 
 const app = new Koa();
 app.use(bodyParser());
 
-dotenv.config();
-
 const webhooks = new Map();
 
+async function callWebhook(url, headers, method) {
+  log(`Send to: ${method} ${url}`);
+  try {
+    await axios(url, { headers, method });
+    webhooks.delete(url);
+  } catch (err) {
+    error(`Failed to fetch ${url}`);
+    console.error(err);
+  }
+}
+
 app.use((ctx) => {
-  if (ctx.method !== 'POST') {
+  if (ctx.method !== "GET") {
     return;
   }
-  console.log(ctx.request.body)
-  const { url, headers, method } = ctx.request.body;
 
-  console.log('Url: ', url);
+  const { url, headers, method } = ctx.request.query;
 
-  if (webhooks.has(url)) {
-    clearTimeout(webhooks.get(url));
+  log(`INCOMING REQUEST: ${method} ${url}`);
+
+  if (!url) {
+    ctx.body = JSON.stringify({
+      success: false,
+      message: "url is required",
+    });
+    return;
+  }
+
+  if (!method) {
+    ctx.body = JSON.stringify({
+      success: false,
+      message: "method is required",
+    });
+    return;
+  }
+
+  let didSendNow = false;
+  const existingCall = webhooks.get(url);
+  if (existingCall) {
+    clearTimeout(existingCall.timeout);
+
+    if (existingCall.firstCall < Date.now() - DEBOUNCE_TIME_MS) {
+      log("timeout interval reached, sending now");
+      callWebhook(url, headers, method);
+      didSendNow = true;
+    }
   }
 
   const timeout = setTimeout(() => {
-    console.log('Send to: ', url)
-    axios(url, { headers, method }).then(() => {
-      webhooks.delete(url);
-    }).catch(err => {
-      console.error(`Failed to fetch ${url}`, err);
-    });
-  }, process.env.DEBOUNCE_TIME_S * 1000);
+    callWebhook(url, headers, method);
+  }, DEBOUNCE_TIME_MS);
 
-  webhooks.set(url, timeout);
-  ctx.body = JSON.stringify({ success: true, message: `Sending request in ${process.env.DEBOUNCE_TIME_S}s` })
-})
+  webhooks.set(url, {
+    firstCall: existingCall?.firstCall || Date.now(),
+    timeout,
+  });
+  ctx.body = JSON.stringify({
+    success: true,
+    message: `Sending request in ${DEBOUNCE_TIME_S}s${didSendNow ? " and just now" : ""}`,
+  });
+});
 
-app.listen(process.env.PORT);
+app.listen(PORT);
+log(`Server running on port ${PORT}`);
